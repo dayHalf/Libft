44301684097dd2951d9a580363798d3c301f9417
oojm1210@naver.com <jeoh@cx1r5s4.42seoul.kr>
2023-01-14 03:51:50 +0900

    calloc updating

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeoh <jeoh@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: jeoh <jeoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 00:06:26 by jeoh              #+#    #+#             */
-/*   Updated: 2022/12/27 01:31:09 by jeoh             ###   ########.fr       */
+/*   Updated: 2023/01/14 01:05:58 by jeoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*res;
+
+	res = malloc(count * size);
+	if (!res)
+		return (0);
+	ft_bzero(ret, count * size);
+	return (res);
+}
